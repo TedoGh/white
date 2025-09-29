cef369ae3f842d0be4a5282dfd5b019f037b4c4b
Tedo Ghachava <ghachavatedo@gmail.com>
2025-09-29 21:53:51 +0400

    efef

--- app/manifest/page.tsx
+++ app/manifest/page.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import Faq from "../components/Faq";
 
-const data = [
+interface FaqData {
+  id: string;
+  title: string;
+  description: string;
+}
+
+const data: FaqData[] = [
   {
-    id: 1,
+    id: "1",
     title: "სათაური 1",
     description: "აღწერა 1",
   },
   {
-    id: 2,
+    id: "2",
     title: "სათაური 2",
     description: "აღწერა 2",
   },
   {
-    id: 3,
+    id: "3",
     title: "სათაური 3",
     description: "აღწერა 3",
   },
   {
-    id: 4,
+    id: "4",
     title: "სათაური 4",
     description: "აღწერა 4",
   },
